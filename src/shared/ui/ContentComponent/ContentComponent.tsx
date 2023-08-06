238741b09c6f8cb94a65747c8d8de6111ab7613d
Tatyana <tkoshikova@gmail.com>
2023-08-06 16:25:10 +0700

    refactoring

--- src/shared/ui/ContentComponent/ContentComponent.tsx
+++ src/shared/ui/ContentComponent/ContentComponent.tsx
@@ -1,111 +1,17 @@
 import React from 'react';
-import {
-    GeolocationControl,
-    Map,
-    Placemark,
-    RouteButton,
-    SearchControl,
-    YMaps,
-    ZoomControl
-} from "@pbe/react-yandex-maps";
+import { YMaps } from "@pbe/react-yandex-maps";
 import {Layout} from "antd";
+import MapComponent from "shared/ui/MapComponent/MapComponent";
+import './ContentComponent.scss'
 
 const {Content} = Layout;
 
 function ContentComponent() {
     return (
-        <Content
-            style={{
-                margin: '24px 16px',
-                minHeight: 280,
-
-            }}
-        >
-            <div className="app">
-                <YMaps query={{apikey: '8b29a2bb-5ff5-4ffb-acb6-5596a63f42d3'}}>
-                    <Map className="map"
-                        defaultState={{ center: [54.1, 91.660786], zoom: 8, controls: []
-                        }} modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
-                    >
-                        <Placemark
-                            geometry={[53.73851, 91.591004]}
-                            options={{
-                                balloonPanelMaxMapArea: 0,
-                                preset: 'islands#circleDotIcon',
-                                iconColor: 'red'
-                            }}
-                            properties={{
-                                balloonContent: [
-                                    '<div>',
-                                    '<address>',
-                                    `<strong>Гора Шишка</strong>`,
-                                    '<br/>',
-                                    'Красноярский край, Минусинский район',
-                                    '<br/>',
-                                    ' <a href="#">#природа</a>',
-                                    ' <a href="#">#гора</a>',
-                                    '</address>',
-                                    '<img src="https://avatars.mds.yandex.net/get-altay/2436798/2a00000182791d6ed5b14685a4d830548728/XXXL" class="imgMap"/>',
-                                    '</div>'
-                                ].join(''),
-                            }}
-                        />
-                        <Placemark
-                            geometry={[53.749535, 92.036767]}
-                            options={{
-                                balloonPanelMaxMapArea: 0,
-                                preset: 'islands#circleDotIcon',
-                                iconColor: 'red'
-                            }}
-                            properties={{
-                                balloonContent: [
-                                    '<div>',
-                                    '<address>',
-                                    `<strong>Вечный огонь</strong>`,
-                                    '<br/>',
-                                    'Красноярский край, Минусинский район',
-                                    '<br/>',
-                                    ' <a href="#">#природа</a>',
-                                    ' <a href="#">#вечныйогонь</a>',
-                                    '</address>',
-                                    '<img src=" https://a.d-cd.net/EIAAAgKyJuA-960.jpg" class="imgMap"/>',
-                                    '</div>'
-                                ].join(''),
-                            }}
-                        />
-                        <Placemark
-                            geometry={[54.428320, 91.618282]}
-                            options={{
-                                balloonPanelMaxMapArea: 0,
-                                preset: 'islands#circleDotIcon',
-                                iconColor: 'red'
-                            }}
-                            properties={{
-                                balloonContent: [
-                                    '<div>',
-                                    '<address>',
-                                    `<strong>Гора Тепсей</strong>`,
-                                    '<br/>',
-                                    'Красноярский край, Краснотуранский район',
-                                    '<br/>',
-                                    ' <a href="#">#природа</a>',
-                                    ' <a href="#">#гора</a>',
-                                    '</address>',
-                                    '<img src="https://avatars.mds.yandex.net/get-altay/1588111/2a0000016bef6fff850fc785ea2674c71bc9/XXXL" class="imgMap"/>',
-                                    '</div>'
-                                ].join(''),
-                            }}
-                        />
-                        <RouteButton/>
-                        <SearchControl options={{float: "right"}}/>
-                        <GeolocationControl options={{float: 'right'}} data={{title: 'Моё местоположение'}}
-                        />
-                        <ZoomControl options={{size: "large"}}
-                        />
-
-                    </Map>
-                </YMaps>
-            </div>
+        <Content className="content">
+            <YMaps query={{apikey: '8b29a2bb-5ff5-4ffb-acb6-5596a63f42d3'}}>
+                <MapComponent/>
+            </YMaps>
         </Content>
     );
 }
